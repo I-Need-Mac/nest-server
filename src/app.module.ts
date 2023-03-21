3b2ac69d68f936625bfeb63e2ad9adeaced8aeb1
HanCiHu <chanhui.yu.cnu@gmail.com>
2023-03-21 23:16:48 +0900

    feat: alias 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MedicinesModule } from './medicines/medicines.module';
-import { ConfigModule } from '@nestjs/config';
+
+import { MedicinesModule } from '@medicines/medicines.module';
 
 @Module({
   imports: [
