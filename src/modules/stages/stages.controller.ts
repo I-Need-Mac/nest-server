291495e5521cb7b4f6fe90ecb7cd87f91e36122f
HanCiHu <chanhui.yu.cnu@gmail.com>
2023-04-13 21:33:40 +0900

    feat: dependency injection example

--- src/modules/stages/stages.controller.ts
+++ src/modules/stages/stages.controller.ts
@@ -1,18 +1,23 @@
 import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
 
 import { StagesService } from './stages.service';
+import { UsersService } from '../users/users.service';
 import { CreateStageDto } from './stages.dto';
 import { ApiOperation } from '@nestjs/swagger';
 import { encrypt } from '@utils/security';
 
 @Controller('stage')
 export class StagesController {
-  constructor(private StagesService: StagesService) {}
+  constructor(private StagesService: StagesService, private UsersService: UsersService) {}
 
   @ApiOperation({ summary: '스테이지 시작 시 요청' })
   @Post('/start')
   async create(@Body() createStageData: CreateStageDto) {
     console.log('in router :: ', createStageData);
+
+    // insert stage table data
+    // const stage = await this.StagesService.create(createStageData);
+    // const soul = await this.UsersService.createSoul(createStageData);
 
     return {
       statusCode: HttpStatus.OK,
@@ -20,3 +25,31 @@ export class StagesController {
     };
   }
 }
+
+/*
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { DogsService } from './dogs.service';
+import { Cat } from './interfaces/cat.interface';
+import { Dog } from './interfaces/dog.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(
+    private readonly catsService: CatsService,
+    private readonly dogsService: DogsService,
+  ) {}
+
+  // ... 기존 코드 생략 ...
+
+  @Post('dogs')
+  createDog(@Body() dog: Dog): void {
+    this.dogsService.create(dog);
+  }
+
+  @Get('dogs')
+  findAllDogs(): Dog[] {
+    return this.dogsService.findAll();
+  }
+}
+*/
--- src/modules/stages/stages.module.ts
+++ src/modules/stages/stages.module.ts
@@ -7,9 +7,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DecryptionMiddleware } from './stages.middleware';
 import { GlobalHttpExceptionFilter } from '@/common/errors/globalHttpException.filter';
 import { GlobalValidationPipe } from '@/common/errors/globalValidatiion.pipe';
+// import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Stages])],
+  // providers: [StagesService, UsersService, GlobalHttpExceptionFilter, GlobalValidationPipe],
   providers: [StagesService, GlobalHttpExceptionFilter, GlobalValidationPipe],
   controllers: [StagesController],
 })
