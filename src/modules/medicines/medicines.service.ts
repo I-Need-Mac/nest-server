46c6836d79e2c62ff5a87fc3ef7761d90a51c739
HanCiHu <chanhui.yu.cnu@gmail.com>
2023-05-01 14:40:04 +0900

    fix: 암호화 로직 수정

--- src/modules/medicines/medicines.service.ts
+++ src/modules/medicines/medicines.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { encrypt, decrypt } from '@/common/utils/security';
+import { decryptForTest, encryptForTest } from '@/common/utils/security';
 
 @Injectable()
 export class MedicinesService {
   async encrypt(data: any) {
     console.log(data, 'in encrypt');
-    const data1 = encrypt(JSON.stringify(data.data));
+    const data1 = encryptForTest(JSON.stringify(data.data));
     return { data: data1 };
   }
 
   async decrypt(data: any) {
     console.log(data, 'in decrypt');
-    const data1 = JSON.parse(decrypt(data.data));
+    const data1 = JSON.parse(decryptForTest(data.data));
     return { data: data1 };
   }
 }
