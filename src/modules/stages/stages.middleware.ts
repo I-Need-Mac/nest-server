4380c9ba8291c178a0eb7b019d977ccd5e1210bc
HanCiHu <chanhui.yu.cnu@gmail.com>
2023-05-03 21:03:26 +0900

    fix: middleware encrypt logic

--- src/modules/stages/stages.middleware.ts
+++ src/modules/stages/stages.middleware.ts
@@ -3,38 +3,33 @@ import { Request, Response, NextFunction } from 'express';
 
 import { decrypt } from '@/common/utils/security';
 import HttpStatus from '@/common/types/status';
-import * as BodyParser from 'body-parser';
-import { json } from 'body-parser';
 
 @Injectable()
 export class DecryptionMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    // BodyParser.raw({ type: '*/*' })(req, res, next);
-    // const { data } = req.body;
+    const { data } = req.body;
+    console.log('in middleware', data);
 
-    // console.log('in middleware', data);
-
-    // if (data == null && req.method === 'GET') {
-    //   throw new HttpException('Invalid data', HttpStatus.INVALID_BODY);
-    // }
-    // try {
-    //   const decryptedData = JSON.parse(decrypt(data));
-    //   console.log('in middle ware :: ', decryptedData);
-    //   req.body = decryptedData;
-    //   next();
-    // } catch (e) {
-    //   throw new HttpException('Invalid data', HttpStatus.INVALID_BODY);
-    // }
-
-    // req.body 시 json이 읽히지 않아 임시 조치
-    json({
-      verify: (req: any, res, buffer) => {
-        if (Buffer.isBuffer(buffer)) {
-          const rawBody = Buffer.from(buffer);
-          req['parsedRawBody'] = rawBody;
-        }
-        return true;
-      },
-    })(req, res as any, next);
+    if (data == null) {
+      throw new HttpException('Invalid data', HttpStatus.INVALID_BODY);
+    }
+    try {
+      const decryptedData = JSON.parse(decrypt(data));
+      console.log('in middle ware :: ', decryptedData);
+      req.body = decryptedData;
+      next();
+    } catch (e) {
+      throw new HttpException('Invalid data', HttpStatus.INVALID_BODY);
+    }
+    // // req.body 시 json이 읽히지 않아 임시 조치
+    // json({
+    //   verify: (req: any, res, buffer) => {
+    //     if (Buffer.isBuffer(buffer)) {
+    //       const rawBody = Buffer.from(buffer);
+    //       req['parsedRawBody'] = rawBody;
+    //     }
+    //     return true;
+    //   },
+    // })(req, res as any, next);
   }
 }
