86d2c50088f91d1c34a266ba758978c41db7747c
WooSangPark <74955871+sang4004@users.noreply.github.com>
2023-06-23 16:16:12 +0900

    Feat/#41 soul open api (#45)
    * feat/#42-character-open-api
    
    * feat/ #41 soul_open_api
    
    * fix/clauses created_at add

--- src/modules/souls/souls.controller.ts
+++ src/modules/souls/souls.controller.ts
@@ -1,9 +1,60 @@
-import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Patch } from '@nestjs/common';
 import { SoulsService } from './souls.service';
 import { ApiOperation } from '@nestjs/swagger';
-import { encrypt } from '@utils/security';
+
+import { updateSoulDto } from './souls.dto';
 
 @Controller('souls')
 export class SoulsController {
   constructor(private SoulsService: SoulsService) {}
+
+  @ApiOperation({ summary: '소울 해금' })
+  @Patch('/open')
+  async updateSoul(@Body() data: updateSoulDto) {
+    if (data.flag == 1) {
+      const count = await this.SoulsService.soulCount(data.steam_id, data.saint_soul, data.soul);
+      if (count >= 7) {
+        const soul = await this.SoulsService.unlockUpdate(data.steam_id, data.saint_soul, data.soul);
+        if (soul) {
+          return {
+            statusCode: HttpStatus.OK,
+            message: 'soul update successfully',
+            data: {
+              steam_id: soul.steam_id,
+              saint_soul: soul.saint_soul_type,
+              soul: data.soul,
+            },
+          };
+        } else {
+          return {
+            statusCode: HttpStatus.BAD_REQUEST,
+            message: 'soul update failed',
+          };
+        }
+      }
+    } else if (data.flag == 2) {
+      const soul = await this.SoulsService.unlockUpdate(data.steam_id, data.saint_soul, data.soul);
+      if (soul) {
+        return {
+          statusCode: HttpStatus.OK,
+          message: 'soul update successfully',
+          data: {
+            steam_id: soul.steam_id,
+            saint_soul: soul.saint_soul_type,
+            soul: data.soul,
+          },
+        };
+      } else {
+        return {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'soul update failed',
+        };
+      }
+    } else {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'flag without',
+      };
+    }
+  }
 }
