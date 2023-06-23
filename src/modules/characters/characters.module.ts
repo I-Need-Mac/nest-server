8032f89aa30bb6960a6ed6e44e0c7f7de1e51f2d
WooSangPark <74955871+sang4004@users.noreply.github.com>
2023-06-23 16:15:49 +0900

    feat/#42-character-open-api (#44)

--- src/modules/characters/characters.module.ts
+++ src/modules/characters/characters.module.ts
@@ -3,11 +3,13 @@ import { CharactersController } from './characters.controller';
 import { CharactersService } from './characters.service';
 import { Characters } from './characters.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AssetsService } from '../assets/assets.service';
+import { Assets } from '../assets/assets.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Characters])],
+  imports: [TypeOrmModule.forFeature([Characters, Assets])],
   controllers: [CharactersController],
-  providers: [CharactersService],
-  exports: [CharactersService],
+  providers: [CharactersService, AssetsService],
+  exports: [CharactersService, AssetsService],
 })
 export class CharactersModule {}
