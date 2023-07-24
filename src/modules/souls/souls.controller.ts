c4c595726fe9386c83b2f1f61fe6d53197b0f5f6
HanCiHu <chanhui.yu.cnu@gmail.com>
2023-07-24 13:29:04 +0900

    fix: souls end point

--- src/modules/souls/souls.controller.ts
+++ src/modules/souls/souls.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, HttpStatus, Patch } from '@nestjs/common';
+import { Controller, Body, HttpStatus, Patch } from '@nestjs/common';
 import { SoulsService } from './souls.service';
 import { ApiOperation } from '@nestjs/swagger';
 
@@ -11,7 +11,7 @@ export class SoulsController {
   @ApiOperation({ summary: '소울 해금' })
   @Patch('/open')
   async updateSoul(@Body() data: updateSoulDto) {
-    if (data.flag == 1) {
+    if (data.flag === 1) {
       const count = await this.SoulsService.soulCount(data.steam_id, data.saint_soul, data.soul);
       if (count >= 7) {
         const soul = await this.SoulsService.unlockUpdate(data.steam_id, data.saint_soul, data.soul);
@@ -32,7 +32,7 @@ export class SoulsController {
           };
         }
       }
-    } else if (data.flag == 2) {
+    } else if (data.flag === 2) {
       const soul = await this.SoulsService.unlockUpdate(data.steam_id, data.saint_soul, data.soul);
       if (soul) {
         return {
--- src/modules/souls/souls.module.ts
+++ src/modules/souls/souls.module.ts
@@ -1,13 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 
 import { Souls } from './souls.entity';
 import { SoulsService } from './souls.service';
 import { SoulsController } from './souls.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { GlobalHttpExceptionFilter } from '@/common/errors/globalHttpException.filter';
-import { GlobalValidationPipe } from '@/common/errors/globalValidatiion.pipe';
-// import { UsersService } from '../users/users.service';
+import { DecryptionMiddleware } from '../characters/characters.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Souls])],
@@ -15,4 +12,8 @@ import { GlobalValidationPipe } from '@/common/errors/globalValidatiion.pipe';
   providers: [SoulsService],
   exports: [SoulsService],
 })
-export class SoulsModule {}
+export class SoulsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(DecryptionMiddleware).forRoutes('souls');
+  }
+}
