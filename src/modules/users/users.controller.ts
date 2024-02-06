4c68cdaf6ed197562761eb0876c5d8966fb8c919
WooSangPark <74955871+sang4004@users.noreply.github.com>
2024-02-06 09:51:00 +0900

    fix: 배열 생성 수정

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -69,12 +69,10 @@ export class UsersController {
       Promise.all(soulPromises)
         .then((results) => {
           results.map((raw) => {
-            for (let i = 1; i <= 18; i++) {
-              this.soulProgressCountService.create({
-                steam_id: data.steam_id,
-                souls_id: raw.id,
-              });
-            }
+            this.soulProgressCountService.create({
+              steam_id: data.steam_id,
+              souls_id: raw.id,
+            });
           });
         })
         .catch((error) => {
